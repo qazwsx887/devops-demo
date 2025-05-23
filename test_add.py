f715149b4c3253f00f3587fc81ffc0701251a1ce
qazwsx <lauragu.xy@outlook.com>
2025-05-23 23:52:15 +0800

    refactor: move add.py into myapp module and fix import path

--- test_add.py
+++ test_add.py
@@ -1,8 +1,8 @@
 # test_add.py
 # 这是测试文件，pytest 会自动识别 test_ 开头的文件和函数
 
-from add import add  # 导入我们写的 add 函数
+from myapp.add import add  # 导入我们写的 add 函数
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(1, 2) == 3
     assert add(-1, 1) == 0
